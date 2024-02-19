a0f4cd8601fa33a3dcb98b54f904e462453721b7
Joseph Clyde Mar <clydemar234@gmail.com>
2024-02-19 16:28:09 +0800

    feat: Use the move method of Boids

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1100, 600
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
+CLOCK = pygame.time.Clock()
 
 if __name__ == '__main__':
-    myBoids = [boids.Boid((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame) for _ in range(50)]
+    myBoids = [boids.Boid((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame) for _ in range(5000)]
     run = True
     while run:
         for event in pygame.event.get():
@@ -19,4 +19,6 @@ if __name__ == '__main__':
         WINDOW.fill((0, 0, 0))
         for boid in myBoids:
             boid.draw()
+            boid.move((500, 300))
         pygame.display.update()
+        CLOCK.tick(120)
