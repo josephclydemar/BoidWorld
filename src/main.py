6760a5d00ee74c2eac7e1e22c9097ed8eb4c1914
Joseph Clyde Mar <clydemar234@gmail.com>
2024-02-24 22:09:44 +0800

    feat: Add a Spawner
    A Spawner is a being that produces Boids.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import random
-
 import pygame
+
 import boids
+import spawner
 
 pygame.init()
 pygame.display.set_caption('BoidWorld')
@@ -14,13 +15,19 @@ FONT = pygame.font.SysFont('Serif', 25, bold=True)
 
 
 if __name__ == '__main__':
+    mySpawner = spawner.Spawner(
+                            [random.randint(10, WINDOW_WIDTH-10), random.randint(10, WINDOW_HEIGHT-10)],
+                            (WINDOW_WIDTH, WINDOW_HEIGHT),
+                            WINDOW,
+                            pygame
+                        )
     myBoids = [boids.Boid(
                             [random.randint(10, WINDOW_WIDTH-10), random.randint(10, WINDOW_HEIGHT-10)],
                             (WINDOW_WIDTH, WINDOW_HEIGHT),
                             WINDOW,
                             pygame,
                             show_circles=False
-                        ) for _ in range(100)]
+                        ) for _ in range(10)]
     for boid in myBoids:
         boid.get_neigbors(list(map(lambda b: b.position, list(filter(lambda b: b != boid, myBoids)))))
     run = True
@@ -35,20 +42,33 @@ if __name__ == '__main__':
         boid_count_text = FONT.render(f'Boid Count: {len(myBoids)}', False, (255, 255, 255), (0, 0, 0))
         fps_text = FONT.render(f'FPS: {CLOCK.get_fps()}', False, (255, 255, 255), (0, 0, 0))
         
+        
+        # all_boid_pos = list(map(lambda b: b.drawable_position[0], myBoids))
+        # pygame.draw.lines(WINDOW, (0, 255, 0), True, all_boid_pos, 1)
 
-        # my_key = pygame.key.get_pressed()
+        my_key = pygame.key.get_pressed()
         # # print(my_key[pygame.K_SPACE])
-        # my_pos = pygame.mouse.get_pos()
-        # my_pos = (my_pos[0], WINDOW_HEIGHT-my_pos[1])
+        my_pos = pygame.mouse.get_pos()
+        my_pos = (my_pos[0], WINDOW_HEIGHT-my_pos[1])
         for boid in myBoids:
             boid.draw(CLOCK.get_fps())
-            # boid.move(my_pos)
-        # if my_key[pygame.K_SPACE]:
-        #     myBoids.append(boids.Boid(my_pos,(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame))
-        # if random.randint(500, 1000) % random.randint(100, 499) == 0:
-        #     myBoids.append(boids.Boid((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame))
-        #     myBoids += [boids.Boid((WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame) for _ in range(10)]
-        #     my_pos = (random.randint(100, WINDOW_WIDTH-100), random.randint(100, WINDOW_HEIGHT-100))
+            boid.move((mySpawner.position[0], mySpawner.position[1]))
         
+        mySpawner.draw()
+        
+
+        if my_key[pygame.K_m]:
+            myBoids.append(boids.Boid((mySpawner.position[0], mySpawner.position[1]),(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame))
+        if my_key[pygame.K_w]:
+            mySpawner.move()
+        if my_key[pygame.K_a]:
+            mySpawner.steer(7)
+        if my_key[pygame.K_d]:
+            mySpawner.steer(-7)
+
+        if my_key[pygame.K_SPACE]:
+            mySpawner.burst()
+        else:
+            mySpawner.relax()
         WINDOW.blit(boid_count_text, (10, 10))
         WINDOW.blit(fps_text, (10, 40))
