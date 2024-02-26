7047bc168ac6abdc9f1426a4c89414f56b97b510
Joseph Clyde Mar <clydemar234@gmail.com>
2024-02-26 15:36:52 +0800

    refactor: Use static variable all_boids in Boid class for tracking all boids

--- src/boids.py
+++ src/boids.py
@@ -4,6 +4,7 @@ import math
 import calcs
 
 class Boid:
+    all_boids = []
     """
     * Initialize the Boid
     @window_size
@@ -11,59 +12,68 @@ class Boid:
     @graphics_lib
     @show_circles
     """
-    def __init__(self, position, window_size, window, graphics_lib, show_circles=False):
+    def __init__(self, position, spawner, window_size, window, graphics_lib, show_circles=False):
         self.graphics_lib = graphics_lib
         self.window = window
         self.window_size = window_size
         self.show_circles = show_circles
 
+        self.spawner = spawner
+
         self.color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
 
         self.dt = 1
         
-
+        self.do_cohesion = True
         self.cohesion_radius = random.uniform(95, 105)
-        self.cohesion_burst_speed = random.uniform(5, 7)
+        self.cohesion_burst_speed = random.uniform(7, 8)
         self.cohesion_circle_color = (self.color[0], self.color[1], self.color[2])
 
+        self.do_alignment = True
         self.alignment_radius = random.uniform(65, 75)
         self.alignment_burst_speed = random.uniform(10, 15)
         self.alignment_circle_color = (self.color[0], self.color[1], self.color[2])
 
+        self.do_separation = True
         self.separation_radius = random.uniform(25, 35)
         self.separation_burst_speed = random.uniform(30, 40)
         self.separation_circle_color = (self.color[0], self.color[1], self.color[2])
 
-        self.relaxed_speed = random.uniform(1, 3)
+        self.relaxed_speed = random.uniform(2, 4)
         self.speed_scalar = self.relaxed_speed
+        self.steering_speed = random.uniform(1, 2)
         self.direction_degrees = random.uniform(0, 359)
         self.position = position
 
         self.tail = []
 
-        self.my_neigbors_pos = None
+        self.my_neigbors = None
+
+        Boid.all_boids.append(self)
+
+        for boid in Boid.all_boids:
+            boid.get_neigbors()
     
     def draw(self, fps):
         if fps > 0:
             self.dt = 60 / fps
         new_direction_radian = (math.pi / 180) * self.direction_degrees
         drawable_position = (
-                (self.position[0], self.window_size[1] - self.position[1]),
-                (self.position[0] + 8 * math.cos(new_direction_radian + 120 * (math.pi / 180)), self.window_size[1] - (self.position[1] + 8 * math.sin(new_direction_radian + 120 * (math.pi / 180)))),
-                (self.position[0] + 15 * math.cos(new_direction_radian), self.window_size[1] - (self.position[1] + 15 * math.sin(new_direction_radian))),
-                (self.position[0] + 8 * math.cos(new_direction_radian - 120 * (math.pi / 180)), self.window_size[1] - (self.position[1] + 8 * math.sin(new_direction_radian - 120 * (math.pi / 180)))),
+                (self.position[0] - 12 * math.cos(new_direction_radian), self.window_size[1] - (self.position[1] - 12 * math.sin(new_direction_radian))),
+                (self.position[0] + 6.4 * math.cos(new_direction_radian + 120 * (math.pi / 180)), self.window_size[1] - (self.position[1] + 6.4 * math.sin(new_direction_radian + 120 * (math.pi / 180)))),
+                (self.position[0] + 12 * math.cos(new_direction_radian), self.window_size[1] - (self.position[1] + 12 * math.sin(new_direction_radian))),
+                (self.position[0] + 6.4 * math.cos(new_direction_radian - 120 * (math.pi / 180)), self.window_size[1] - (self.position[1] + 6.4 * math.sin(new_direction_radian - 120 * (math.pi / 180)))),
                )
-        # self.tail.append(drawable_position[0])
-        # if len(self.tail) > 4:
-        #     self.tail.remove(self.tail[0])
-        # if len(self.tail) > 2:
-        #     self.graphics_lib.draw.lines(self.window, self.color, False, self.tail, 1)
+        self.tail.append(drawable_position[0])
+        if len(self.tail) > 4:
+            self.tail.remove(self.tail[0])
+        if len(self.tail) > 2:
+            self.graphics_lib.draw.lines(self.window, self.color, False, self.tail, 1)
         if self.show_circles:
             self.graphics_lib.draw.circle(self.window, self.cohesion_circle_color, drawable_position[0], self.cohesion_radius, 1)
             self.graphics_lib.draw.circle(self.window, self.alignment_circle_color, drawable_position[0], self.alignment_radius, 1)
             self.graphics_lib.draw.circle(self.window, self.separation_circle_color, drawable_position[0], self.separation_radius, 1)
         self.graphics_lib.draw.polygon(self.window, self.color, drawable_position)
-        # self.cohere()
 
 
     """
@@ -86,14 +96,15 @@ class Boid:
     @dest_direction_degrees
     """
     def steer(self, dest_direction_degrees):
-        steering_speed = abs(self.direction_degrees - dest_direction_degrees) / 180
+        # steering_speed = abs(self.direction_degrees - dest_direction_degrees) / 180
         # self.direction_degrees += steering_speed
         if self.direction_degrees > dest_direction_degrees + 10:
             # self.direction_degrees -= steering_speed
-            self.direction_degrees -= 2
+            self.direction_degrees -= self.steering_speed
         elif self.direction_degrees < dest_direction_degrees - 10:
             # self.direction_degrees += steering_speed
-            self.direction_degrees += 2
+            self.direction_degrees += self.steering_speed
+        # self.direction_degrees = dest_direction_degrees
     
 
     def cohere_burst(self):
@@ -127,9 +138,8 @@ class Boid:
     * Receives the positions of all other Boids
     @others
     """
-    def get_neigbors(self, others):
-        # print(self.position, ' -> ', others)
-        self.my_neigbors_pos = others
+    def get_neigbors(self):
+        self.my_neigbors = list(map(lambda boid: boid, list(filter(lambda boid: boid != self, Boid.all_boids))))
     
     """
     * Filters other Boids that are within the passed radius
@@ -137,9 +147,9 @@ class Boid:
     """
     def filter_neigbors_within_radius(self, radius):
         seen_neigbors = []
-        for neigbor_pos in self.my_neigbors_pos:
-            if calcs.calc_distance(neigbor_pos, self.position) <= radius:
-                seen_neigbors.append(neigbor_pos)
+        for neigbor in self.my_neigbors:
+            if calcs.calc_distance(neigbor.position, self.position) <= radius:
+                seen_neigbors.append(neigbor)
         return seen_neigbors
     
     def align(self):
@@ -149,22 +159,4 @@ class Boid:
         pass
 
     def cohere(self):
-        neigbors_within_cohesion = self.filter_neigbors_within_radius(self.cohesion_radius)
-        total_xy = [0, 0]
-        for neigbor_pos in neigbors_within_cohesion:
-            if len(neigbors_within_cohesion) == 0:
-                break
-            total_xy[0] += neigbor_pos[0]
-            total_xy[1] += neigbor_pos[1]
-        if len(neigbors_within_cohesion) != 0:
-            average_close_neigbors_position = (
-                    total_xy[0]/len(self.my_neigbors_pos),
-                    total_xy[1]/len(self.my_neigbors_pos),
-                )
-            self.cohere_burst()
-            self.move(average_close_neigbors_position)
-        else:
-            my_pos = self.graphics_lib.mouse.get_pos()
-            my_pos = (my_pos[0], self.window_size[1]-my_pos[1])
-            self.relax()
-            self.move(my_pos)
+        pass
--- src/main.py
+++ src/main.py
@@ -10,9 +10,11 @@ pygame.display.set_caption('BoidWorld')
 WINDOW_WIDTH, WINDOW_HEIGHT = 1500, 750
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
-FONT = pygame.font.SysFont('Serif', 25, bold=True)
+FONT = pygame.font.SysFont('Serif', 20, bold=True)
 
 
+do_cohesion = False
+do_alignment = False
 
 if __name__ == '__main__':
     mySpawner = spawner.Spawner(
@@ -21,44 +23,44 @@ if __name__ == '__main__':
                             WINDOW,
                             pygame
                         )
-    myBoids = [boids.Boid(
-                            [random.randint(10, WINDOW_WIDTH-10), random.randint(10, WINDOW_HEIGHT-10)],
-                            (WINDOW_WIDTH, WINDOW_HEIGHT),
-                            WINDOW,
-                            pygame,
-                            show_circles=False
-                        ) for _ in range(100)]
-    for boid in myBoids:
-        boid.get_neigbors(list(map(lambda b: b.position, list(filter(lambda b: b != boid, myBoids)))))
+    myBoids = boids.Boid.all_boids
     run = True
     while run:
-        pygame.display.update()
-        CLOCK.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     run = False
+                
+                if event.key == pygame.K_o:
+                    for boid in myBoids:
+                        boid.do_cohesion = not(boid.do_cohesion)
+                if event.key == pygame.K_p:
+                    for boid in myBoids:
+                        boid.do_alignment = not(boid.do_alignment)
+        
         WINDOW.fill((0, 0, 0))
+
         boid_count_text = FONT.render(f'Boid Count: {len(myBoids)}', False, (255, 255, 255), (0, 0, 0))
         fps_text = FONT.render(f'FPS: {CLOCK.get_fps()}', False, (255, 255, 255), (0, 0, 0))
-        
-        
-        # all_boid_pos = list(map(lambda b: b.drawable_position[0], myBoids))
-        # pygame.draw.lines(WINDOW, (0, 255, 0), True, all_boid_pos, 1)
 
         my_key = pygame.key.get_pressed()
-        # # print(my_key[pygame.K_SPACE])
-        my_pos = pygame.mouse.get_pos()
-        my_pos = (my_pos[0], WINDOW_HEIGHT-my_pos[1])
+        # my_pos = pygame.mouse.get_pos()
+        # my_pos = (my_pos[0], WINDOW_HEIGHT-my_pos[1])
+        
         for boid in myBoids:
             boid.draw(CLOCK.get_fps())
-            boid.move((mySpawner.position[0], mySpawner.position[1]))
-        
         mySpawner.draw()
         
 
-        if my_key[pygame.K_m]:
-            myBoids.append(boids.Boid((mySpawner.position[0], mySpawner.position[1]),(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW, pygame))
+        if my_key[pygame.K_q]:
+            boids.Boid(
+                        (mySpawner.position[0], mySpawner.position[1]), 
+                        mySpawner,
+                        (WINDOW_WIDTH, WINDOW_HEIGHT),
+                        WINDOW,
+                        pygame,
+                        show_circles=False
+                    )
         if my_key[pygame.K_w]:
             mySpawner.move()
         if my_key[pygame.K_a]:
@@ -70,5 +72,21 @@ if __name__ == '__main__':
             mySpawner.burst()
         else:
             mySpawner.relax()
+        
+
+        # if my_key[pygame.K_o]:
+        #     for boid in myBoids:
+        #         boid.do_cohesion = not(boid.do_cohesion)
+        # if my_key[pygame.K_p]:
+        #     for boid in myBoids:
+        #         boid.do_alignment = not(boid.do_alignment)
+        
+        
+        
+
         WINDOW.blit(boid_count_text, (10, 10))
         WINDOW.blit(fps_text, (10, 40))
+
+
+        pygame.display.update()
+        CLOCK.tick(60)
